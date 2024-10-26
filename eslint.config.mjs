3112ccda0479563905986ec884c857263856f8ac
Muhumuza Deus <157320582+MDeus-ai@users.noreply.github.com>
2024-10-26 18:38:53 +0300

    fix3

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,16 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        process: "readonly" // Add the process global variable
+      },
+    },
+  },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
 ];
